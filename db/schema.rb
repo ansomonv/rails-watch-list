c00b114fdcf09d626c2673bb6bf4934b5b437811
Anne-sophie Monville <annesophiemonville@gmail.com>
2022-06-03 12:03:06 +0200

    rails watch day1 done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_124759) do
+ActiveRecord::Schema.define(version: 2022_06_03_091205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.text "comment"
+    t.string "comment"
     t.bigint "movie_id", null: false
     t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
